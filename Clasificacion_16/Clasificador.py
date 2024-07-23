dce61e62e571a33688e8579ca4f5e8a375d02547
Ana Cuevas de Cózar <anacuevasdecozar@gmail.com>
2024-07-23 13:54:24 +0200

    Clasificador primeros 8 channels de los 16

--- Clasificacion_16/Clasificador.py
+++ Clasificacion_16/Clasificador.py
@@ -23,12 +23,22 @@ print(f'Tamaño del dataset: {df.shape}')
 # Mezclamos los datos para conseguir una muestra más homogénea al evaluar solo algunos
 df = shuffle(df)
 
+
 # Separar características (X) de etiquetas (y)
-X = df.iloc[:, :-1].values  # Todas las filas, todas las columnas excepto la última
+#Tomando todas las filas como características
+#X_16= df.iloc[:, :-1].values  # Todas las filas, todas las columnas excepto la última
+
+# Especificamos las columnas que deseamos usar
+columnas_8 = ['Channel_1', 'Channel_2','Channel_3','Channel_4','Channel_5','Channel_6','Channel_7','Channel_8','quat1','quat2','quat3','quat4','acc1','acc2','acc3','gyro1','gyro2','gyro3']
+
+# Elegimos solo las primeras 8 columnas
+X_8 = df[columnas_8].values  # Selecciona solo las columnas deseadas
 y = df.iloc[:, -1].values   # Todas las filas, solo la última columna
 
 # Separamos los datos en datos de entrenamiento y de test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#X_train, X_test, y_train, y_test = train_test_split(X_16, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X_8, y, test_size=0.3, random_state=42)
+
 
 # Estandarizar los datos
 scaler = StandardScaler()
@@ -63,11 +73,11 @@ param_grid = {
 print('Comienza el training')
 start_time = time.time()  # Iniciar el cronómetro
 # Crear y entrenar el modelo con GridSearchCV para encontrar los mejores hiperparámetros
-grid = GridSearchCV(model, param_grid, cv=3, refit=True, verbose=2, n_jobs=10)
+grid = GridSearchCV(model, param_grid, cv=3, refit=True, verbose=2, n_jobs=7)
 grid.fit(X_train_sample, y_train_sample)
 
 # Guardamos el modelo
-joblib.dump(grid, 'clasificador_svm_poly_total.pkl')
+joblib.dump(grid, 'clasificador_svm_poly_8_de_16.pkl')
 
 # Predecir etiquetas para los datos de prueba
 y_pred = grid.predict(X_test)
